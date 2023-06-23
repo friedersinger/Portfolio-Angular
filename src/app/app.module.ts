b2e3dfe9108515fcc854431d5fac0ef18ffd9392
Frieder Singer <friedersinger@users.noreply.github.com>
2023-06-23 10:00:03 +0200

    added comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { BackgroundComponent } from './background/background.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    BackgroundComponent,
+    ContactComponent,
+    FooterComponent,
+    HeaderComponent,
+    ImprintComponent,
+    IntroductionComponent,
+    PortfolioComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
